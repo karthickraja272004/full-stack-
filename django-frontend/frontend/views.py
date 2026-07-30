19210be256157b5d1e0f7a0aac93a2cc4327bcf1
karthickraja272004 <skraja27102004@gmail.com>
2026-07-30 13:21:16 +0530

    last

--- django-frontend/frontend/views.py
+++ django-frontend/frontend/views.py
@@ -3,30 +3,10 @@ from django.conf import settings
 from django.shortcuts import render
 
 
+from django.http import HttpResponse
+
 def all_users(request):
-    try:
-        print("=" * 50)
-        print("Inside all_users()")
-        print("BACKEND_URL:", settings.BACKEND_URL)
-
-        response = requests.get(settings.BACKEND_URL + "/users")
-
-        print("Status Code:", response.status_code)
-        print("Response Text:", response.text)
-
-        data = response.json()
-
-        print("JSON Data:", data)
-
-        context = {
-            "users": data["users"]
-        }
-
-        return render(request, "home.html", context)
-
-    except Exception as e:
-        print("ERROR in all_users():", repr(e))
-        raise
+    return HttpResponse("Hello from Django")
 
 
 def user_detail(request, user_id):
